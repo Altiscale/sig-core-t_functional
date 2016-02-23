f1e025243acb1a846c27883bc101f3126229a650
karthik <karthik@altiscale.com>
2016-02-23 19:11:12 +0530

    SI-559 Excluding failing tests

--- runtests.sh
+++ runtests.sh
@@ -35,7 +35,8 @@ if [ $# -gt 0 ]; then
   t_Process <(/usr/bin/find ./tests/$1/ -type f|sort -t'/' )
 else
   t_Process <(/usr/bin/find ./tests/0_*/ -type f|sort -t'/' )
-  t_Process <(/usr/bin/find ./tests/p_*/ -type f|sort -t'/' )
+  #t_Process <(/usr/bin/find ./tests/p_*/ -type f|sort -t'/' )
+  t_Process <(/usr/bin/find ./tests/p_*/ -type f ! -path "./tests/p_yum/*" ! -path "./tests/p_anaconda/*" ! -path "./tests/p_centos-release/*" ! -path "./tests/p_diffutils/*" ! -path "./tests/p_iptables/*" ! -path "./tests/p_kernel/*" ! -path "./tests/p_libvirt/*" ! -path "./tests/p_only/*" ! -path "./tests/p_ruby/*" ! -path "./tests/p_yum-plugin-fastestmirror/*" |sort -t'/' )
   t_Process <(/usr/bin/find ./tests/r_*/ -type f|sort -t'/' )
   t_Process <(/usr/bin/find ./tests/z_*/ -type f|sort -t'/' )
 fi
