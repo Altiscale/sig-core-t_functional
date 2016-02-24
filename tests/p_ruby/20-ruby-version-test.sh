46defd8b23b77dcd16626bb577b6e9b27a15b122
karthik <karthik@altiscale.com>
2016-02-24 19:26:10 +0530

    OPS-10067 fixing ruby tests

--- tests/p_ruby/20-ruby-version-test.sh
+++ tests/p_ruby/20-ruby-version-test.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-# Author: Nikhil Lanjewar <nikhil@lanjewar.com>
-# Author: Sahil Muthoo <sahilm@thoughtworks.com>
-# Author: Sahil Aggarwal <sahilagg@gmail.com>
-# Author: Saager Mhatre <saager.mhatre@gmail.com>
-#	  Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - Check version of ruby."
 
 if [ "$centos_ver" = "7" ] ; then
-  ruby -v | grep -q '2.0'
-  ret_val=$?
+  # We don't use centos 7
+  ret_val=1
 elif [ "$centos_ver" = "6" ] ; then
-  ruby -v | grep -q '1.8.7'
+  ruby -v | grep -q '2.0.0'
   ret_val=$?
 else
-  ruby -v | grep -q '1.8.5'
-  ret_val=$?
+  # We don't use centos 5
+  ret_val=1
 fi
 
 t_CheckExitStatus $ret_val
--- tests/p_ruby/40-ri-version-test.sh
+++ tests/p_ruby/40-ri-version-test.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
-# Author: Nikhil Lanjewar <nikhil@lanjewar.com>
-# Author: Sahil Muthoo <sahilm@thoughtworks.com>
-# Author: Sahil Aggarwal <sahilagg@gmail.com>
-# Author: Saager Mhatre <saager.mhatre@gmail.com>
-#	  Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - Check version of ruby ri."
 
-#allready prepared just in case versions should change between C5 and C6
 if [ "$centos_ver" = "7" ] ; then
-  ri -v | grep -q '4.0'
-  ret_val=$?
+  # We don't use centos 7
+  ret_val=1
 else
-  ri -v | grep -q '1.0.1'
+  ri -v | grep -q '4.0.0'
   ret_val=$?
 fi
 
